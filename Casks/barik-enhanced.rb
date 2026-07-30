da7f7b59ec4ada3d697bcd91a3c31bbc5bd490d0
Mateo Cerquetella <mateocerquetella@hotmail.com>
2026-07-30 15:33:18 -0400

    bump cask to 1.4.0

--- Casks/barik-enhanced.rb
+++ Casks/barik-enhanced.rb
@@ -1,13 +1,13 @@
 cask "barik-enhanced" do
-  version "1.3.8"
-  sha256 "d7d5311ab2124c64a06fbb2e434bee78899672d4eae04eddbba167271a413f42"
+  version "1.4.0"
+  sha256 "2d816ce62c5a9e616e928f81a14516dcd93f2d8a9334e8c4d99230d1e8115141"
 
   url "https://github.com/MateoCerquetella/barik-enhanced/releases/download/v#{version}/BarikEnhanced.zip"
   name "Barik Enhanced"
   desc "Custom menu bar with 20+ configurable widgets"
   homepage "https://github.com/MateoCerquetella/barik-enhanced"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "BarikEnhanced.app"
 
